91d59c8ddb49503f8186a84063f574108009aeb5
LehmanBrother <chrislehman1991@gmail.com>
2018-10-14 17:05:09 -0500

    Added index.ejs

---
+++ controllers/artistController.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+const Artists = require('../models/artistModel');
+
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const artistsController = require('./controllers/artists');
+const artistsController = require('./controllers/artistController');
 require('./db/db');
 
 //middleware
