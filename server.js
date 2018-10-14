5a7a5c42f040f605686af03f47f79292e453438e
LehmanBrother <chrislehman1991@gmail.com>
2018-10-14 16:58:10 -0500

    Added server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const artistsController = require('./controllers/artists');
+require('./db/db');
+
+//middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app,.use(methodOverride('_method'));
+app.use('/artists', artistsController);
+
+app.get('/',(req, res) => {
+	res.send('This is the server responding to a get request');
+})
+
+app.listen(3000, () => {
+	console.log('Server is listening on port 3000');
+})
