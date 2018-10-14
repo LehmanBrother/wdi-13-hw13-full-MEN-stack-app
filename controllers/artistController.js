eaca584cb386a69d6c0725ca37ed12f33800acb6
LehmanBrother <chrislehman1991@gmail.com>
2018-10-14 17:06:46 -0500

    Index route

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -2,3 +2,15 @@ const express = require('express');
 const router = express.Router();
 const Artists = require('../models/artistModel');
 
+//index route
+router.get('/',(req, res) => {
+	Artists.find({},(err, allArtists) => {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render('index.ejs', {
+				artists: allArtists
+			})
+		}
+	})
+})
